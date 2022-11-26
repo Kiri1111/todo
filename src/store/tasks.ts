ce1b67e4a9b92561ce1c69d4d40466678b707ab2
Кирилл <94886658+Kiri1111@users.noreply.github.com>
2022-11-26 18:05:18 +0300

    reducer

--- src/store/tasks.ts
+++ src/store/tasks.ts
@@ -2,3 +2,26 @@ export const calculator = (a: number, b: number) => a + b
 export const calculatorMin = (a: number, b: number) => a - b
 export const calculatorSub = (a: number, b: number) => a * b
 export const calculatorDiv = (a: number, b: number) => a / b
+
+type StateType = number
+
+
+type ActionType = {
+    type: 'SUM' | 'MIN' | 'SUB' | 'DIV'
+    n: number
+}
+
+const allReducer = (state: StateType, action: ActionType) => {
+    switch (action.type) {
+        case 'SUM':
+            return state + action.n
+        case 'MIN':
+            return state - action.n
+        case'SUB':
+            return state * action.n
+        case 'DIV':
+            return state / action.n
+        default:
+            return state
+    }
+}

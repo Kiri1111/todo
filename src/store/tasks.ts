7220837bd2b929301532c73aac3a38ed80344504
Кирилл <94886658+Kiri1111@users.noreply.github.com>
2022-12-08 21:12:39 +0300

    reducer(js native 9)

--- src/store/tasks.ts
+++ src/store/tasks.ts
@@ -25,3 +25,83 @@ const allReducer = (state: StateType, action: ActionType) => {
             return state
     }
 }
+
+
+// const [tasks, dispatchTasks] = useReducer(tasksReducer, [
+//     {id: v1(), title: "HTML&CSS", isDone: true},
+//     {id: v1(), title: "JS", isDone: true},
+//     {id: v1(), title: "ReactJS", isDone: false},
+//     {id: v1(), title: "Rest API", isDone: false},
+//     {id: v1(), title: "GraphQL", isDone: false},
+// ])
+//
+// function removeTask(id: string) {
+//     // let filteredTasks = tasks.filter(t => t.id != id);
+//     // setTasks(filteredTasks);
+//     dispatchTasks(removeTaskAC(id))
+// }
+//
+// function addTask(title: string) {
+//     // let task = { id: v1(), title: title, isDone: false };
+//     // let newTasks = [task, ...tasks];
+//     // setTasks(newTasks);
+//     dispatchTasks(addTaskAC(title))
+// }
+
+
+// import {TaskType} from "../Todolist";
+// import {v1} from "uuid";
+//
+// export const tasksReducer = (state: TaskType[], action: TsarType) => {
+//     switch (action.type) {
+//         case'REMOVE-TASK': {
+//             //  let filteredTasks = tasks.filter(t => t.id != id);
+//             return state.filter(el => el.id !== action.payload.id)
+//         }
+//         case 'ADD-TASK': {
+//             let task = {id: v1(), title: action.payload.title, isDone: false};
+//
+//             return [task, ...state]
+//         }
+//         default:
+//             return state
+//     }
+// }
+//
+// type TsarType = RemoveTaskACType | addTaskACACType
+// export type RemoveTaskACType = ReturnType<typeof removeTaskAC>
+//
+//
+// export const removeTaskAC = (id: string) => {
+//     return {
+//         type: 'REMOVE-TASK',
+//         payload: {id}
+//     } as const
+// }
+// export type addTaskACACType = ReturnType<typeof addTaskAC>
+// export const addTaskAC = (title: string) => {
+//     return {
+//         type: 'ADD-TASK',
+//         payload: {
+//             title
+//         }
+//     } as const
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
